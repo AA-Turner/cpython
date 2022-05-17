5a32300ab5ff475ef33ebce85ddbd8a4d2947d05
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2022-05-17 19:28:39 +0100

    Get Furo to run

--- Doc/conf.py
+++ Doc/conf.py
@@ -65,14 +65,7 @@ master_doc = 'contents'
 # -----------------------
 
 # Use our custom theme.
-html_theme = 'python_docs_theme'
-html_theme_path = ['tools']
-html_theme_options = {
-    'collapsiblesidebar': True,
-    'issues_url': '/bugs.html',
-    'license_url': '/license.html',
-    'root_include_title': False   # We use the version switcher instead.
-}
+html_theme = 'furo'
 
 # Short title used e.g. for <title> HTML tags.
 html_short_title = '%s Documentation' % release
@@ -83,14 +76,6 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # Path to find HTML templates.
 templates_path = ['tools/templates']
-
-# Custom sidebar templates, filenames relative to this file.
-html_sidebars = {
-    # Defaults taken from https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_sidebars
-    # Removes the quick search block
-    '**': ['localtoc.html', 'relations.html', 'customsourcelink.html'],
-    'index': ['indexsidebar.html'],
-}
 
 # Additional templates that should be rendered to pages.
 html_additional_pages = {
